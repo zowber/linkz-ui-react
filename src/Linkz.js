4bb2c67574962ac296ec11822f7faa1115d32b07
Andy Bright <andy@zowber.com>
2018-01-25 19:00:38 +0000

    Messin' with SemanticUI

--- src/Linkz.js
+++ src/Linkz.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import jQuery from 'jquery';
+import { Container, Segment, Divider, Header, Form, Item, Button} from 'semantic-ui-react'
 
 class Linkz extends Component {
 
@@ -8,6 +9,16 @@ class Linkz extends Component {
     this.state = {
       linkz: []
     };
+  }
+
+  componentWillMount() {
+    jQuery.ajax({
+      url: "http://192.168.1.127:3000/linkz",
+      type: "get",
+      success: (res) => {
+        this.setState({ linkz: res.reverse() })
+      }
+    });
   }
   
   _getLinkz() {  
@@ -24,10 +35,10 @@ class Linkz extends Component {
     });  
   }
 
-  _addLink (name, url) {
+  _addLink (url, name) {
     const link = {
-      name,
-      url
+      url,
+      name
     };
     jQuery.ajax({
       url: "http://192.168.1.127:3000/linkz",
@@ -46,67 +57,73 @@ class Linkz extends Component {
       success: "TODO: Update state after delete."
     });
   }
-
-  componentWillMount() {
-    jQuery.ajax({
-      url: "http://192.168.1.127:3000/linkz",
-      type: "get",
-      success: (res) => {
-        this.setState({ linkz: res })
-      }
-    });
-  }
   
   render() {
     const linkz = this._getLinkz();
+
+    var headerStyle = { marginTop: '2em', marginBottom: '1em' }
+
     return (
-      <div className="linkz-list">
-        <LinkForm addLink={this._addLink.bind(this)} />
-        <p>{linkz.length} links</p>
-        {linkz}
-      </div>
+      <Container>
+        <Header as='h1' textAlign='center' style={headerStyle}>Linkz</Header>
+        <Segment>
+          <LinkForm addLink={this._addLink.bind(this)} />
+        </Segment>
+        <Segment>
+          <Item.Group divided>
+            {linkz}
+          </Item.Group>
+        </Segment>
+      </Container>
     );
   }
 }
 
 class LinkForm extends Component {
-  render() {
-    return (
-        <form className="link-form" onSubmit={this._handleSubmit.bind(this)}>
-          <input type="text" id="url" name="url" placeholder="URL" ref={(url) => this._url = url} />
-          <input type="text" id="name" name="name" placeholder="Name" ref={(name) => this._name = name }/>
-          <button type="submit">Add link</button>
-        </form>
-    );
+  state = { url: '', name: '' }
+
+  handleChange = (e, { name, value }) => this.setState({ [name]: value })
+
+  handleSubmit = () => {
+    const { url, name } = this.state
+    this.props.addLink(url, name);
+    this.setState({ url: '', name: '' })
   }
 
-  _handleSubmit(event) {
-    event.preventDefault();
+  render() {
+    const { url, name } = this.state
 
-    let name = this._name;
-    let url = this._url;
-
-    this.props.addLink(name.value, url.value);
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        <Form.Group widths='equal'>
+          <Form.Input type="text" name="url" value={url} label="URL" placeholder="http://example.com/" onChange={this.handleChange}/>
+          <Form.Input type="text" name="name" value={name} label="Name" placeholder="Billy's Cool Site" onChange={this.handleChange} />
+        </Form.Group>
+        <Form.Button>Add link</Form.Button>
+      </Form>
+    );
   }
 }
 
 class Link extends Component {
-  render() {
-    return (
-      <div>
-        <ul>
-          <li>Name: {this.props.name}</li>
-          <li>Url: <a href={this.props.url}>{this.props.url}</a></li>
-          <li>Created: {this.props.created}</li>
-          <li><a href={this.props.id} onClick={this._handleDelete.bind(this)}>Delete</a></li>
-        </ul>
-      </div>
-    );
+  handleDelete = () => {
+    this.props.onDelete(this.props.id);
   }
 
-  _handleDelete(event) {
-    event.preventDefault();
-    this.props.onDelete(this.props.id);
+  render() {
+    return (
+      <Item>
+        <Item.Content>
+          <Item.Header>{this.props.name}</Item.Header>
+          <Item.Meta>
+            <span><a href={this.props.url}>{this.props.url}</a></span>
+          </Item.Meta>
+          <Item.Extra>
+            <Button floated='right' onClick={this.handleDelete}>Delete</Button>
+          </Item.Extra>
+        </Item.Content>
+      </Item>
+    );
   }
 }
 
