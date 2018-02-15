ef541b6821ab19673973ff0cd92900121224953b
Andy Bright <andy@zowber.com>
2018-02-15 16:16:37 +0000

    Update .gitignore

--- src/Linkz.js
+++ src/Linkz.js
@@ -1,45 +1,14 @@
-import React, { Component } from 'react';
-import { Grid, Container, Segment, Menu, Header, Input, Item} from 'semantic-ui-react'
-
-import Link from './Link'
-import LinkForm from './LinkForm'
-
-import data from './data'
+import React, {component} from 'react';
 
 class Linkz extends Component {
 
-  state = {
-    linkz: [],
-    filterString: ''
-  }
 
-  componentWillMount() {
-    data.getLinkzFromServer(res => 
-      this.setState({ linkz: res })
-    );
-  }
-
-  handleAddLink = (link) => {
-    data.addLink(link, res => {
-      this.setState({ linkz: this.state.linkz.concat(res) })
-    });
-  }
-
-  handleDeleteLink = (linkId, res) => {
-    data.deletLinkFromServer(linkId, res => {
-      this.setState({ linkz: res })
-    });
-  }
-
-  handleFilterStringChange = (e, {value}) => {
-    this.setState({filterString: value})
-  }
-  
   render() {
-    const linkz = this.state.linkz.sort((a, b) => {
-      return new Date(b.Created_date) - new Date(a.Created_date)
-    });
-    const filteredLinks = linkz.filter(link => (
+    const sortedLinkz = this.state.linkz.sort((a, b) => (
+      new Date(b.Created_date) - new Date(a.Created_date)
+      )
+    );
+    const filteredLinks = sortedLinkz.filter(link => (
         link.name.toLowerCase().includes(
           this.state.filterString.toLowerCase()
         )
@@ -58,48 +27,10 @@ class Linkz extends Component {
           onDeleteLink={this.handleDeleteLink}
         />
       );
-    }); 
-
-    let headerStyle = { marginTop: '2em', marginBottom: '1em', textAlign: 'center' }
+    });
 
     return (
-      <Container>
-        <Grid stackable>
-          <Grid.Column width={16}>
-            <Header style={headerStyle} as='h1' content='Linkz' />
-          </Grid.Column>
-          <Grid.Column width={4}>
-            <Segment>
-              <LinkForm onAddLink={this.handleAddLink} />
-            </Segment>
-          </Grid.Column>
-          <Grid.Column width={12}>
-            <Menu attached='top'>
-              <Menu.Item>
-                {linkComponents.length} links
-              </Menu.Item>
-              <Menu.Item
-                position='right'
-              >
-                <Input
-                  icon='search' 
-                  transparent
-                  placeholder='Search'
-                  value={this.state.filterString}
-                  onChange={this.handleFilterStringChange}
-                />
-              </Menu.Item>
-            </Menu>
-            <Segment attached='bottom'>
-              <Item.Group divided>
-                {linkComponents}
-              </Item.Group>
-            </Segment>
-          </Grid.Column>
-        </Grid>
-      </Container>
-    );
+      {linkComponents}
+    )
   }
 }
-
-export default Linkz
