a09fabe7eefdfeb4a9a5cac9d300361d358ffe50
Andy Bright <andybright@outlook.com>
2019-01-07 11:31:26 +0000

    Formatting

--- src/App.js
+++ src/App.js
@@ -1,59 +1,61 @@
-import React, { Component } from 'react';
-import { Grid, Container, Segment, Menu, Header, Input, Item} from 'semantic-ui-react'
+import React, { Component } from "react";
+import {
+  Grid,
+  Container,
+  Segment,
+  Menu,
+  Header,
+  Input,
+  Item
+} from "semantic-ui-react";
 
-import Link from './Link'
-import LinkForm from './LinkForm'
+import Link from "./Link";
+import LinkForm from "./LinkForm";
 
-import data from './data'
+import data from "./data";
 
 class App extends Component {
-
   state = {
     linkz: [],
-    filterString: ''
-  }
+    filterString: ""
+  };
 
   componentWillMount() {
-    data.getLinkzFromServer(res => 
-      this.setState({ linkz: res })
-    );
+    data.getLinkzFromServer(res => this.setState({ linkz: res }));
   }
 
-  handleAddLink = (link) => {
+  handleAddLink = link => {
     data.addLink(link, res => {
-      this.setState({ linkz: this.state.linkz.concat(res) })
+      this.setState({ linkz: this.state.linkz.concat(res) });
     });
-  }
+  };
 
-  handleUpdateLink = (link) => {
+  handleUpdateLink = link => {
     data.updateLink(link, res => {
-      this.setState({ linkz: res })
-    })
-  }
-  
+      this.setState({ linkz: this.state.linkz.map() });
+    });
+  };
 
   handleDeleteLink = (linkId, res) => {
-    data.deletLinkFromServer(linkId, res => {
-      this.setState({ linkz: res })
+    data.deleteLinkFromServer(linkId, res => {
+      this.setState({ linkz: res });
     });
-  }
+  };
 
-  handleFilterStringChange = (e, {value}) => {
-    this.setState({filterString: value})
-  }
-  
+  handleFilterStringChange = (e, { value }) => {
+    this.setState({ filterString: value });
+  };
+
   render() {
-    const linkz = this.state.linkz.sort((a, b) => {
-      return new Date(b.Created_date) - new Date(a.Created_date)
-    });
-    const filteredLinks = linkz.filter(link => (
-        link.name.toLowerCase().includes(
-          this.state.filterString.toLowerCase()
-        )
-      )
+    const linkz = this.state.linkz.sort((a, b) => 
+      new Date(b.Created_date) - new Date(a.Created_date)
+    );
+    
+    const filteredLinks = linkz.filter(link => 
+      link.name.toLowerCase().includes(this.state.filterString.toLowerCase())
     );
 
-    const linkComponents = filteredLinks.map((link) => {
+    const linkComponents = filteredLinks.map(link => {
       return (
         <Link
           key={link._id}
@@ -66,42 +68,43 @@ class App extends Component {
           onDeleteLink={this.handleDeleteLink}
         />
       );
-    }); 
+    });
 
-    let headerStyle = { marginTop: '2em', marginBottom: '1em', textAlign: 'center' }
+    let headerStyle = {
+      marginTop: "2em",
+      marginBottom: "1em",
+      textAlign: "center"
+    };
 
     return (
       <Container>
         <Grid stackable>
           <Grid.Column width={16}>
-            <Header style={headerStyle} as='h1' content='Linkz' />
+            <Header style={headerStyle} as="h1" content="Linkz" />
           </Grid.Column>
           <Grid.Column width={4}>
             <Segment>
-              <LinkForm onSaveLink={this.handleAddLink} saveButtonText='Add link' />
+              <LinkForm
+                onSaveLink={this.handleAddLink}
+                saveButtonText="Add link"
+              />
             </Segment>
           </Grid.Column>
           <Grid.Column width={12}>
-            <Menu attached='top'>
-              <Menu.Item>
-                {linkComponents.length} links
-              </Menu.Item>
-              <Menu.Item
-                position='right'
-              >
+            <Menu attached="top">
+              <Menu.Item>{linkComponents.length} links</Menu.Item>
+              <Menu.Item position="right">
                 <Input
-                  icon='search' 
+                  icon="search"
                   transparent
-                  placeholder='Search'
+                  placeholder="Search"
                   value={this.state.filterString}
                   onChange={this.handleFilterStringChange}
                 />
               </Menu.Item>
             </Menu>
-            <Segment attached='bottom'>
-              <Item.Group divided>
-                {linkComponents}
-              </Item.Group>
+            <Segment attached="bottom">
+              <Item.Group divided>{linkComponents}</Item.Group>
             </Segment>
           </Grid.Column>
         </Grid>
@@ -110,4 +113,4 @@ class App extends Component {
   }
 }
 
-export default App
+export default App;
