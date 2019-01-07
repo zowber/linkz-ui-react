9001ca8725714564b7369c9d7dc8e857f6fb779e
Andy Bright <andy@zowber.com>
2019-01-07 21:25:26 +0000

    DRY

--- src/data.js
+++ src/data.js
@@ -1,71 +1,55 @@
-const API_HOST = process.env.REACT_APP_API_HOST || 'http://localhost:3000'
-console.log('API_HOST: ' + API_HOST)
+const API_HOST = process.env.REACT_APP_API_HOST || "http://localhost:3000"
+console.log("API_HOST: " + API_HOST)
+
+const checkStatus = response => {
+  if (response.status >= 200 && response.status < 300) {
+    return Promise.resolve(response)
+  } else {
+    return Promise.reject(new Error(response.statusText))
+  }
+}
 
 const data = {
   getLinkzFromServer: success => {
-    fetch(API_HOST + '/linkz/')
-      .then(response => {
-        if (response.status >= 200 && response.status < 300) {
-          return Promise.resolve(response)
-        } else {
-          return Promise.reject(new Error(response.statusText))
-        }
-      })
+    fetch(API_HOST + "/linkz/")
+      .then(checkStatus)
       .then(response => response.json())
       .then(success)
       .catch(error => console.log(error))
   },
   addLink: (link, success) => {
-    fetch(API_HOST + '/linkz/', {
-      method: 'post',
+    fetch(API_HOST + "/linkz/", {
+      method: "post",
       body: JSON.stringify(link),
       headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
+        Accept: "application/json",
+        "Content-Type": "application/json"
       }
     })
-      .then(response => {
-        if (response.status >= 200 && response.status < 300) {
-          return Promise.resolve(response)
-        } else {
-          return Promise.reject(new Error(response.statusText))
-        }
-      })
+      .then(checkStatus)
       .then(response => response.json())
       .then(success)
       .catch(error => console.log(error))
   },
   updateLink: (link, success) => {
-    fetch(API_HOST + '/linkz/' + link.id, {
-      method: 'put',
+    fetch(API_HOST + "/linkz/" + link.id, {
+      method: "put",
       body: JSON.stringify(link),
       headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
+        Accept: "application/json",
+        "Content-Type": "application/json"
       }
     })
-      .then(response => {
-        if (response.status >= 200 && response.status < 300) {
-          return Promise.resolve(response)
-        } else {
-          return Promise.reject(new Error(response.statusText))
-        }
-      })
+      .then(checkStatus)
       .then(response => response.json())
       .then(success)
       .catch(error => console.log(error))
   },
   deleteLinkFromServer: (linkId, success) => {
-    fetch(API_HOST + '/linkz/' + linkId, {
-      method: 'delete'
+    fetch(API_HOST + "/linkz/" + linkId, {
+      method: "delete"
     })
-      .then(response => {
-        if (response.status >= 200 && response.status < 300) {
-          return Promise.resolve(response)
-        } else {
-          return Promise.reject(new Error(response.statusText))
-        }
-      })
+      .then(checkStatus)
       .then(response => response.json())
       .then(success)
       .catch(error => console.log(error))
